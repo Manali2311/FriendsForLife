793593cf52c6906473c858702fe824cb8ec4f18b
Manali2311 <manalisharma2311@gmail.com>
2020-06-02 22:14:31 +0530

    ImageDone

--- src/Landing/Card/Card-component/card-component.jsx
+++ src/Landing/Card/Card-component/card-component.jsx
@@ -5,7 +5,7 @@ import 'react-bootstrap';
 export const Card =(p) =>(
     <div className="card-compo card">
  <div className="gradient-cover"></div>
-    <img className="imggvalue card-img-top" src={process.env.PUBLIC_URL + p.Friends.imgurl} alt="img" height={p.Friends.height}/>
+    <img className="imggvalue card-img-top" src={process.env.PUBLIC_URL + p.Friends.imgurl} alt={process.env.PUBLIC_URL + "/Images/6844.jpg"} height={p.Friends.height}/>
  <div className="detail-text card-body">
  <i className="name card-title">{p.Friends.name}</i>
  <p className="mail card-text">{p.Friends.email}</p> 
